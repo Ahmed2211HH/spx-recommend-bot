2c99f9e37384b563eb130d9e927f37e66eb56589
Ahmed2211HH <a20111892006@gmail.com>
2025-04-21 14:01:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ async def contact(u: Update, cx: ContextTypes.DEFAULT_TYPE):
     if row and datetime.fromisoformat(row[0]) > datetime.now():
         await u.message.reply_text("✅ أنت مشترك")
     else:
-        await u.message.reply_text(f"يبدو أنك غير مشترك.
+       await u.message.reply_text(f"أرسل رقمك للتأكد من الاشتراك.")
 اشترك هنا: {STORE_LINK}")
 
 async def receipt(u: Update, cx: ContextTypes.DEFAULT_TYPE):
