e9b233d6e6e4c0fe7e1c878eefe74ecc5c5abfc1
Ahmed2211HH <a20111892006@gmail.com>
2025-05-01 23:41:58 +0300

    Update install_playwright.py

--- install_playwright.py
+++ install_playwright.py
@@ -1,11 +1,2 @@
-import asyncio
 import subprocess
-from playwright.async_api import async_playwright
-
-async def main():
-    subprocess.run(["playwright", "install", "chromium"])
-    async with async_playwright() as p:
-        print("✅ Playwright and Chromium installed!")
-
-if __name__ == "__main__":
-    asyncio.run(main())
+subprocess.run(["playwright", "install", "chromium"])
